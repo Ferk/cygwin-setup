c8265eeb3fe0e9edd0fb464abe1090dde3317730
Ferk <Ferkv@Cooly.(none)>
2014-03-31 00:32:37 +0200

    improved sudo to not fail on standard sudo options

--- setup.d/sudo.sh
+++ setup.d/sudo.sh
@@ -2,6 +2,15 @@
 
 cat >/bin/sudo <<EOF
 #!/usr/bin/bash
+while getopts "vAknSa:g:p:u:lbEHPC:c:r:t:" opt
+do
+	case "\$opt" in
+		u) 	user="\$OPTARG" ;;
+		b)  ;; # cygstart always opens new window
+		*)	echo "sudo: no support for '-\$opt'" ;;
+	esac
+done
+shift \$((OPTIND-1))
 cygstart --action=runas "\$@"
 EOF
 chmod +x /bin/sudo

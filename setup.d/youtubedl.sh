b8a34d9af2eae814460cbb5fc149b926fbf15ce4
Ferk <Ferkv@Cooly.(none)>
2014-09-21 21:54:04 +0200

    Do not install youtubedl if already there

--- setup.d/youtubedl.sh
+++ setup.d/youtubedl.sh
@@ -2,7 +2,12 @@
 
 URL=https://yt-dl.org/downloads/2014.02.05/youtube-dl.exe
 
-cd /bin/
-wget --no-check-certificate "$URL"
-chmod +x /bin/*.exe
 
+if which youtube-dl
+then
+	echo "youtube-dl is already installed!"
+else
+	cd /bin/
+	wget --no-check-certificate "$URL"
+	chmod +x /bin/*.exe
+fi

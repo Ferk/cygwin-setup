c225a606431259958ab1a57f6775d76aa61b64dd
Fernando Carmona Varo <ferkiwi@gmail.com>
2014-09-27 12:22:52 +0200

    Use unzip directly for unpacking

--- setup.d/sysinternals.sh
+++ setup.d/sysinternals.sh
@@ -5,6 +5,6 @@ cd /tmp
 [ -f "SysinternalsSuite.zip" ] || \
  wget "http://download.sysinternals.com/files/SysinternalsSuite.zip"
 
-yes "n" | aunpack -E -X /bin/ "SysinternalsSuite.zip"
+unzip -n -d /bin/ "SysinternalsSuite.zip"
 
 chmod +x /bin/*.exe

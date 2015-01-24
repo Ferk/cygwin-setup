8460af60c150d7bb9bfa297f4e76870894d90b28
Fernando Carmona Varo <ferkiwi@gmail.com>
2015-01-24 15:34:25 +0100

    cleanup

--- setup.d/chere.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# Add a "Bash prompt here" context menu when right clicking on
-# A directory.
-
-chere -im -t mintty
--- setup.d/hwclock-UTC.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-# Sets the system to use the hardware clock as UTC time, and only set the local timezone
-# in the OS level (like it's done in most UNIX systems).
-# This way we avoid constantly changing the time when dual booting.
-
-regtool -wd set /HKLM/SYSTEM/CurrentControlSet/Control/TimeZoneInformation/RealTimeIsUniversal "1"
